8825aecc4c48a59d1556a863ede653ebc8598f88
youidelh <youssef-wld@hotmail.fr>
2016-05-08 16:40:01 +0200

    Cell release

---
+++ src/de/htwg/tictactoe/model/Cell.java
@@ -0,0 +1,64 @@
+package de.htwg.tictactoe.model;
+
+public class Cell {
+		
+	private int row;
+	private int column;
+	private String value;
+	
+	/**
+	 * the Constructor
+	 * @param row number
+	 * @param column number
+	 */
+	public Cell(int row, int column) {
+		this.row = row;
+		this.column = column;
+		this.value = "";
+	}
+	
+	/**
+	 * Get the Value of the Cell
+	 * @return the Value of the Cell.
+	 */
+	public String getValue(){
+		return value;
+	}
+	
+	/**
+	 * Sets the new Value to the Cell
+	 * @param value to be set
+	 */
+	public void setValue(String value){
+		this.value = value;
+	}
+	
+	/**
+	 * 	Check if Cell is set
+	 * @return true if the Cell has a value other then ""
+	 */
+	public boolean isSet() {
+		return value != "";
+	}
+
+	/**Get the row
+	 * @return row number
+	 */
+	public int getRow() {
+		return row;
+	}
+	
+	/**
+	 * Get the column
+	 * @return column number
+	 */
+	public int getColumn() {
+		return column;
+	}
+	
+	@Override
+	public String toString(){
+		return "Cell("+row+","+column+")";
+	}
+
+}
