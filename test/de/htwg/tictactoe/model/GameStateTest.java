9eae74675f66b360bff21c4937fc14b48d27c8db
youidelh <youssef-wld@hotmail.fr>
2016-05-29 20:26:19 +0200

    remove unused tests

--- test/de/htwg/tictactoe/model/GameStateTest.java
+++ test/de/htwg/tictactoe/model/GameStateTest.java
@@ -74,57 +74,6 @@ public class GameStateTest {
 		assertFalse(state.checkDiagOfAllGrids(0, 1, 0));
 		assertTrue(state.checkDiagOfAllGrids(1, 1, 1));
 	}
-	@Test
-	public void testDiagStartingFromMiddle(){
-		assertFalse(state.diagStartingFromMiddle(0, 2));
-		assertFalse(state.diagStartingFromMiddle(1, 0));
-		assertFalse(state.diagStartingFromMiddle(0, 1));
-		assertTrue(state.diagStartingFromMiddle(0, 1));
-		assertFalse(state.diagStartingFromMiddle(1, 1));
-		assertFalse(state.diagStartingFromMiddle(3, -2));
-	}
-	@Test
-	public void testDiagStartingFromMiddle2(){
-		assertFalse(state.diagStartingFromMiddle(2, 1));
-		assertFalse(state.diagStartingFromMiddle(1, 2));
-		assertFalse(state.diagStartingFromMiddle(5, -2));
-		assertTrue(state.diagStartingFromMiddle(2, 1));
-	} 
-	@Test 
-	public void testDiagStartingFromMiddle3(){
-		assertFalse(state.diagStartingFromMiddle(2, 1));
-		assertFalse(state.checkDiagOfAllGrids(1, 0, 1));
-		assertTrue(state.diagStartingFromMiddle(2, 1));
-	}
- 
-	@Test
-	public void testDiagStartingFromMiddle4(){ 
-	assertFalse(state.checkDiagOfAllGrids(2, 1, 1));
-	assertFalse(state.checkDiagOfAllGrids(2, 2, 0));
-	assertTrue(state.diagStartingFromMiddle(1, 0));
-	}
-	@Test 
-	public void testHelpForDiagOfSpecifiedRows(){
-		assertFalse(state.diagOfSpecifiedRows(0, 0, 2));
-		assertFalse(state.diagOfSpecifiedRows(3, 0, 1));
-
-	}
-	@Test 
-	public void testDiagOfSpecifiedRows(){
-		assertFalse(state.helpForDiagOfSpecifiedRows(1, 1, 1, 1));
-		assertFalse(state.helpForDiagOfSpecifiedRows(1, 1, 1, 1));
-		assertTrue(state.helpForDiagOfSpecifiedRows(1, 1, 1, 1));
-		assertFalse(state.helpForDiagOfSpecifiedRows(1, 1, 3, 1));
-		assertFalse(state.helpForDiagOfSpecifiedRows(1, 4, 1, 1));
-		assertTrue(state.helpForDiagOfSpecifiedRows(1, 0, 1, 2));
-	}
-	@Test 
-	public void testDiagOfSpecifiedRows2(){	
-
-		assertFalse(state.checkDiagOfAllGrids(1, 0, 0));
-		assertFalse(state.checkDiagOfAllGrids(1, 1, 1));
-		assertTrue(state.helpForDiagOfSpecifiedRows(1, 1, 0, 2));
-	}
 	
 	@Test 
 	public void testCheckForWin(){
@@ -140,9 +89,5 @@ public class GameStateTest {
 		assertTrue(state.checkForWin(2, 2, 0));
 		//check diagonal of all grids
 		assertTrue(state.checkForWin(2, 2, 2));
-	}
-	@Test
-	public void testCheckGivenGrid(){
-		assertEquals(0, state.checkGivenGrid(1));
 	}
 }
