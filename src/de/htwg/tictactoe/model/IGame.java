824107bb3984b354d4d4835b3a355be25bc8e44c
dafranze <dafranze@htwg-konstanz.de>
2016-06-17 19:35:10 +0200

    Minor fixes

--- src/de/htwg/tictactoe/model/IGame.java
+++ src/de/htwg/tictactoe/model/IGame.java
@@ -6,25 +6,29 @@ package de.htwg.tictactoe.model;
 public interface IGame {
 
     /**
-     * sets all values ​​of the current game state to the initial state
+     * Sets all values ​​of the current game state to the initial state.
      */
     void resetGame();
 
     /**
-     * checks the value in one cell of tree possible cells, whether the value is set
+     * Checks the value in one cell of tree possible cells,
+     * whether the value is set.
      *
-     * @param row the row of the current move
+     * @param row    the row of the current move
      * @param column the column of the current move
-     * @param grid the grid with the row and the column of the current move
+     * @param grid   the grid with the row and the column
+     *               of the current move
      *
-     * @return if the cell is set, the method will return true, otherwise false.
+     * @return       if the cell is set, the method will
+     *               return true, otherwise false.
      */
     boolean cellIsSet(int row, int column, int grid);
 
     /**
-     * gives the grids of the current game state with all current values of the grids
+     * Gives the grids of the current game state with all
+     * current values of the grids.
      *
-     * @return a array with all grids of the current game
+     * @return       a array with all grids of the current game
      */
     Grid[] getGrids();
 
